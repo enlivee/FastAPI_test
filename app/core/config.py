8085e6c6afb27670234d55c834af89c90b6bda9d
Egor Matyushin <e.m.matyushin@gmail.com>
2026-06-05 17:25:51 +0300

    обновление организации

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,5 @@
 from dataclasses import dataclass
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
 @dataclass(frozen=True)
 class Settings:
     DATABASE_URL: str
@@ -14,16 +10,3 @@ def get_settings():
         DATABASE_URL="postgresql+psycopg://postgres:admin@127.0.0.1:15432/postgres",
         cors_allowed_origins = ["http://localhost:3000"]
     )
-
-
-settings = get_settings()
-engine = create_engine(settings.DATABASE_URL)
-Sessionlocal = sessionmaker(bind=engine)
-
-
-def get_db():
-    db = Sessionlocal()
-    try:
-        yield db
-    finally:
-        db.close()
--- app/db/session.py
+++ app/db/session.py
@@ -1,1 +1,17 @@
-from app.core.config import Sessionlocal, engine
+from app.core.config import get_settings
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+settings = get_settings()
+engine = create_engine(settings.DATABASE_URL)
+Sessionlocal = sessionmaker(bind=engine)
+
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
