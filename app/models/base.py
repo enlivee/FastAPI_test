de48d7f989d8a5b1fbc53165da707212a0ca1cc7
Egor Matyushin <e.m.matyushin@gmail.com>
2026-06-05 17:19:09 +0300

    Модели

---
+++ app/models/base.py
@@ -0,0 +1,6 @@
+from uuid import uuid4
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid4()))
