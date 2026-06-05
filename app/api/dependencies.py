f0172ec80ee3330f0c47759d7c2fa29f577424ed
Egor Matyushin <e.m.matyushin@gmail.com>
2026-06-05 17:20:23 +0300

    Зависимости

---
+++ app/api/dependencies.py
@@ -0,0 +1,14 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from app.core.config import get_db
+from app.services.category import CategoryService
+from app.services.task import TaskService
+
+
+def get_task_service(db: Session = Depends(get_db)):
+    return TaskService(db)
+
+
+def get_category_service(db: Session = Depends(get_db)):
+    return CategoryService(db)
