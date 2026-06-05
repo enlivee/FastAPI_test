5e60631f1fd8eeeb6af021a4b3c929486aa165ac
Egor Matyushin <e.m.matyushin@gmail.com>
2026-06-05 17:19:44 +0300

    Репозитории

---
+++ app/repositories/category.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from app.models.category import CategoryORM
+
+
+class CategoryRepository():
+    def __init__(self, db: Session) -> None:
+        self.db = db
+
+    def get_all(self) -> list[CategoryORM]:
+        return self.db.scalars(select(CategoryORM)).all()
+
+    def get_by_id(self, category_id: str) -> CategoryORM:
+        return self.db.get(CategoryORM, category_id)
+
+    def create(self, name: str) -> CategoryORM:
+        new_category = CategoryORM(name=name)
+        self.db.add(new_category)
+        return new_category
+
+    def delete(self, CategoryORM) -> None:
+        self.db.delete(CategoryORM)
